26eacef7898cca03897512c29a0208cfd6e152a2
kirilllzaitsev <kzaitsev88@gmail.com>
2025-05-30 12:21:16 +0200

    i:match pred-gt if gt provided

--- memotr/models/runtime_tracker.py
+++ memotr/models/runtime_tracker.py
@@ -1,7 +1,9 @@
 # Copyright (c) Ruopeng Gao. All Rights Reserved.
+import copy
 import os
 from typing import Dict, List
 
+from memotr.models.matcher import HungarianMatcher
 import torch
 from memotr.structures.track_instances import TrackInstances
 
@@ -20,6 +22,7 @@ class RuntimeTracker:
         motion_max_length: int = 5,
         visualize: bool = False,
         use_dab: bool = True,
+        matcher:HungarianMatcher=None
     ):
         self.det_score_thresh = det_score_thresh
         self.track_score_thresh = track_score_thresh
@@ -31,8 +34,9 @@ class RuntimeTracker:
         self.motion_max_length = motion_max_length
         self.motions: Dict[Motion] = {}
         self.use_dab = use_dab
+        self.matcher=matcher
 
-    def update(self, model_outputs: dict, tracks: List[TrackInstances]):
+    def update(self, model_outputs: dict, tracks: List[TrackInstances], batch=None):
         assert len(tracks) == 1
         model_outputs["scores"] = logits_to_scores(model_outputs["pred_logits"])
         n_dets = len(model_outputs["det_query_embed"])
@@ -98,6 +102,98 @@ class RuntimeTracker:
         )
         new_tracks.labels = torch.max(new_tracks.scores, dim=-1).indices
 
+        if batch is not None and len(new_tracks) > 0 and self.matcher is not None:
+            targets=batch['target']
+            batch_size=len(targets)
+            tracked_instances=copy.deepcopy(tracks)
+
+            # 3. Get the detection results in current frame.
+            detection_res = {
+                "pred_logits": model_outputs["pred_logits"][:, :n_dets],
+                "pred_boxes": model_outputs["pred_bboxes"][:, :n_dets],
+                "rot": model_outputs["rot"][:, :n_dets],
+                "t": model_outputs["t"][:, :n_dets],
+            }
+
+            gt_trackinstances = TrackInstances.init_tracks(
+                batch,
+                hidden_dim=new_tracks.hidden_dim,
+                num_classes=new_tracks.num_classes,
+            )
+            for b in range(batch_size):
+                gt_trackinstances[b].ids = targets[b]["track_ids"]
+                gt_trackinstances[b].labels = targets[b]["labels"]
+                gt_trackinstances[b].boxes = targets[b]["boxes"]
+                gt_trackinstances[b].rots = targets[b]["rot"]
+                gt_trackinstances[b].other_attrs["mesh_pts"] = targets[b][
+                    "mesh_pts"
+                ]
+                gt_trackinstances[b].other_attrs["mesh_bbox"] = targets[b][
+                    "mesh_bbox"
+                ]
+                gt_trackinstances[b].ts = targets[b]["t"]
+
+            gt_ids_to_idx = []
+            for b in range(len(tracked_instances)):
+                gt_ids_to_idx.append(
+                    {
+                        gt_id.item(): gt_idx
+                        for gt_idx, gt_id in enumerate(gt_trackinstances[b].ids)
+                    }
+                )
+            num_disappeared_tracked_gts = 0
+            for b in range(len(tracked_instances)):
+                gt_idx = []
+                if len(tracked_instances[b]) > 0:
+                    for gt_id in tracked_instances[b].ids.tolist():
+                        if gt_id in gt_ids_to_idx[b]:
+                            gt_idx.append(gt_ids_to_idx[b][gt_id])
+                        else:
+                            gt_idx.append(-1)
+                            num_disappeared_tracked_gts += 1
+                tracked_instances[b].matched_idx = torch.as_tensor(
+                    data=gt_idx, dtype=tracked_instances[b].matched_idx.dtype
+                )
+            # 4.+ Filter the gts that not in the tracked instances:
+            # gt_full_idx = []
+            # untracked_gt_trackinstances = []
+            # for b in range(len(tracked_instances)):
+            #     gt_full_idx.append(torch.arange(start=0, end=len(gt_trackinstances[b])))
+            # for b in range(len(tracked_instances)):
+            #     idx_bool = torch.ones(size=gt_full_idx[b].shape, dtype=torch.bool)
+            #     for i in tracked_instances[b].matched_idx:
+            #         if i.item() >= 0:
+            #             idx_bool[i.item()] = False
+            #     untracked_gt_trackinstances.append(gt_trackinstances[b][idx_bool])
+            untracked_gt_trackinstances = gt_trackinstances
+
+            # 5. Use Hungarian algorithm to matching.
+            matcher_res = self.matcher(
+                outputs=detection_res, targets=untracked_gt_trackinstances, use_focal=True
+            )
+            matcher_res = [list(mr) for mr in matcher_res]
+
+            # match new_tracks and targets
+            b=0
+            new_tracks_idxes_numeric = torch.tensor([i for i, x in enumerate(new_tracks_idxes) if x])
+            output_idx, gt_idx = matcher_res[b]
+            matched_idx_mask = torch.isin(output_idx, new_tracks_idxes_numeric)
+            # TODO (wrong matching wrt track queries)
+            if len(gt_idx)>1:
+                gt_idx=gt_idx[matched_idx_mask]
+            # output_idx = output_idx[matched_idx_mask]
+            gt_ids = untracked_gt_trackinstances[b].ids[gt_idx] # suppresses false pos
+            gt_idx = torch.as_tensor(
+                [gt_ids_to_idx[b][gt_id.item()] for gt_id in gt_ids], dtype=torch.long
+            )
+            # new_tracks.ids = gt_ids
+            new_tracks.matched_idx = gt_idx
+            new_tracks.other_attrs["mesh_pts"] = untracked_gt_trackinstances[
+                b
+            ].other_attrs["mesh_pts"][gt_idx]
+            new_tracks.other_attrs["mesh_bbox"] = untracked_gt_trackinstances[
+                b
+            ].other_attrs["mesh_bbox"][gt_idx]
         # We do not use this post-precess motion module in our final version,
         # this will bring a slight improvement,
         # but makes un-elegant.
@@ -105,6 +201,7 @@ class RuntimeTracker:
             new_tracks.last_appear_boxes = model_outputs["pred_bboxes"][0][:n_dets][
                 new_tracks_idxes
             ]
+        # V1
         ids = []
         for i in range(len(new_tracks)):
             ids.append(self.max_obj_id)
